8f9f7887f1b8f4a891ec4195e5ca969aeaae0a6a
anthonyyip51 <anthonyyip51@gmail.com>
2019-04-01 10:15:30 -0700

    first try

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080
+
+app.use(express.static(__dirname));
+
+
+
+app.get("/", function(req, res) {
+	res.render("index.html");
+})
+
+app.get("/social", function(req, res) {
+	res.sendFile(__dirname+"/social.html");
+})
+
+app.get("/about", function(req, res) {
+	res.sendFile(__dirname+"/about.html");
+})
+
+app.get("/influencers", function(req, res) {
+	res.sendFile(__dirname+"/influencers.html");
+})
+
+app.get("/contact", function(req, res) {
+	res.sendFile(__dirname+"/contact.html");
+})
+
+
+app.listen(port, function() {
+	console.log("app running");
+})
